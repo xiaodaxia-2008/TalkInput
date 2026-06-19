b20f968ac70f2888e65c77c7199885579e1ae114
xiaodaxia <xiaozisheng2008@hotmail.com>
2026-06-19 23:22:10 +0800

    Revert spdlog pattern changes, keep default format

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 int main(int argc, char *argv[])
 {
     spdlog::set_level(spdlog::level::debug);
-    spdlog::set_pattern("[%s:%# %!] [%^%l%$] %v");
     spdlog::flush_on(spdlog::level::debug);
     SPDLOG_DEBUG("starting application");
 
